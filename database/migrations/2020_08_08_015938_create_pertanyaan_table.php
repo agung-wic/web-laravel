98d209efb09b48dde672d82db8f400e42b037eb9
Agung Wicaksono <wicaksonoagung05@gmail.com>
2020-08-08 13:23:00 +0700

    tugas migration

--- database/migrations/2020_08_08_015938_create_pertanyaan_table.php
+++ database/migrations/2020_08_08_015938_create_pertanyaan_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreatePasswordResetsTable extends Migration
+class CreatePertanyaanTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,10 +13,12 @@ class CreatePasswordResetsTable extends Migration
      */
     public function up()
     {
-        Schema::create('password_resets', function (Blueprint $table) {
-            $table->string('email')->index();
-            $table->string('token');
-            $table->timestamp('created_at')->nullable();
+        Schema::create('pertanyaan', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('judul',45);
+            $table->string('isi',255);
+            $table->date('tanggal_dibuat');
+            $table->date('tanggal_diprbarui');
         });
     }
 
@@ -27,6 +29,6 @@ class CreatePasswordResetsTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('password_resets');
+        Schema::dropIfExists('pertanyaan');
     }
 }
